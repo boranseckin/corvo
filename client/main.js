5c77027a3c727b33938bca52bcec79db39c48399
Boran Seckin <me@boranseckin.com>
2019-06-22 11:19:01 -0400

    Added geoIPLookup

--- client/main.js
+++ client/main.js
@@ -1,6 +1,14 @@
+/* eslint-disable meteor/no-session */
 import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
+import { Tracker } from 'meteor/tracker';
 
 import '../lib/routes.js';
 
 Meteor.startup(() => {
+    Tracker.autorun(() => {
+        Meteor.call('ip', function(error, result) {
+            Session.set('clientIP', result.ip);
+        });
+    });
 });
