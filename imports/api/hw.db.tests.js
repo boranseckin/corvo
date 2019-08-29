110162d134791e03a5758140abef857f83f7079e
Boran Seckin <me@boranseckin.com>
2019-08-29 16:39:28 -0400

    Added tests for all methods

--- imports/api/hw.db.tests.js
+++ imports/api/hw.db.tests.js
@@ -3,17 +3,18 @@
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
 import { assert } from 'chai';
+import { resetDatabase } from 'meteor/xolvio:cleaner';
 
 import HW from './hw.db.js';
 
 if (Meteor.isServer) {
-    describe('DB-HW', () => {
-        describe('methods', () => {
+    describe('DB-HW', function() {
+        describe('methods', function() {
             const userID = Random.id();
             let hwID;
 
             beforeEach(() => {
-                HW.remove({});
+                resetDatabase();
                 hwID = HW.insert({
                     alias: 'Test',
                     subject: 'Math',
@@ -26,7 +27,80 @@ if (Meteor.isServer) {
                 });
             });
 
-            it('can delete hw', () => {
+            it('can edit hw -> alias', function() {
+                const editHW = Meteor.server.method_handlers['hw.edit'];
+                const invocation = { userID };
+
+                editHW.apply(invocation, [hwID, 'alias', 'editedTest']);
+
+                const query = HW.find({ _id: hwID }).fetch();
+                assert.equal(query[0].alias, 'editedTest');
+            });
+
+            it('can edit hw -> subject', function() {
+                const editHW = Meteor.server.method_handlers['hw.edit'];
+                const invocation = { userID };
+
+                editHW.apply(invocation, [hwID, 'subject', 'CS']);
+
+                const query = HW.find({ _id: hwID }).fetch();
+                assert.equal(query[0].subject, 'CS');
+            });
+
+            it('can edit hw -> dueDate', function() {
+                const editHW = Meteor.server.method_handlers['hw.edit'];
+                const invocation = { userID };
+
+                const date = new Date(Date.now() + (60000 * 3));
+
+                editHW.apply(invocation, [hwID, 'dueDate', date]);
+
+                const query = HW.find({ _id: hwID }).fetch();
+                assert.deepEqual(date, query[0].dueDate);
+            });
+
+            it('can edit hw -> submitMerhod', function() {
+                const editHW = Meteor.server.method_handlers['hw.edit'];
+                const invocation = { userID };
+
+                editHW.apply(invocation, [hwID, 'submitMethod', 'Paper']);
+
+                const query = HW.find({ _id: hwID }).fetch();
+                assert.equal(query[0].submitMethod, 'Paper');
+            });
+
+            it('can edit hw -> partners', function() {
+                const editHW = Meteor.server.method_handlers['hw.edit'];
+                const invocation = { userID };
+
+                editHW.apply(invocation, [hwID, 'partners', ['TP1', 'TP2']]);
+
+                const query = HW.find({ _id: hwID }).fetch();
+                assert.equal(query[0].partners[0], 'TP1');
+                assert.equal(query[0].partners[1], 'TP2');
+            });
+
+            it('can edit hw -> description', function() {
+                const editHW = Meteor.server.method_handlers['hw.edit'];
+                const invocation = { userID };
+
+                editHW.apply(invocation, [hwID, 'description', 'LoremDescriptionIpsum']);
+
+                const query = HW.find({ _id: hwID }).fetch();
+                assert.equal(query[0].description, 'LoremDescriptionIpsum');
+            });
+
+            it('can complete hw', function() {
+                const completeHW = Meteor.server.method_handlers['hw.complete'];
+                const invocation = { userID };
+
+                completeHW.apply(invocation, [hwID]);
+
+                const query = HW.find({ _id: hwID }).fetch();
+                assert.equal(query[0].isCompleted, true);
+            });
+
+            it('can delete hw', function() {
                 const deleteHW = Meteor.server.method_handlers['hw.remove'];
                 const invocation = { userID };
 
