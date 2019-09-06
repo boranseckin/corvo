2eafd23fd48317b884537fc6928be471fd88ed2c
Boran Seckin <me@boranseckin.com>
2019-09-06 17:43:16 -0400

    Added 404 page

--- imports/ui/RouteNotFound.jsx
+++ imports/ui/RouteNotFound.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
+import { Result } from 'antd';
 
 class RouteNotFound extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
         };
     }
 
     render() {
         return (
             <div>
-                <h1>Route Not Found!</h1>
+                <Result
+                    status="404"
+                    title="404"
+                    subTitle="Sorry, the page you visited does not exist."
+                />
             </div>
         );
     }
