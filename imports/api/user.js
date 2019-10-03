1ab04e824408b5b459717abf9384a84ea0f88255
Boran Seckin <me@boranseckin.com>
2019-10-02 21:21:15 -0400

    Added user api

---
+++ imports/api/user.js
@@ -0,0 +1,18 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+import { Accounts } from 'meteor/accounts-base';
+
+Meteor.methods({
+    'user.insert'(username, email, password) {
+        check(username, String);
+        check(email, String);
+        check(password, String);
+
+        Accounts.createUser({
+            username,
+            email,
+            password,
+        });
+    },
+});
--- server/main.js
+++ server/main.js
@@ -1,9 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 import '../imports/api/db.js';
+import '../imports/api/user.js';
 import '../imports/api/url.db.js';
 import '../imports/api/hw.db.js';
 import '../imports/api/hw.class.db.js';
 
 Meteor.startup(() => {
+    Accounts.emailTemplates.from = 'Corvo Verify <verify@boranseckin.com>';
+    process.env.MAIL_URL = Meteor.settings.mail.smtp;
 });
