bf82e0dfb3e7203e6ae274013fd6d35bfc296f03
Boran Seckin <me@boranseckin.com>
2019-11-24 17:03:24 -0500

    Added footer with current release version

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,13 @@
+import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import { Tracker } from 'meteor/tracker';
+
+import { Layout } from 'antd';
 
 import Navbar from './Navbar.jsx';
+
+const { Footer } = Layout;
 
 export default class App extends Component {
     static propTypes = {
@@ -16,11 +22,25 @@ export default class App extends Component {
         super(props);
 
         this.state = {
+            version: null,
         };
+    }
+
+    componentDidMount() {
+        const track = Tracker.autorun(() => {
+            Meteor.call('CORVO_VERSION', (err, res) => {
+                this.setState({
+                    version: res,
+                }, () => {
+                    track.invalidate();
+                });
+            });
+        });
     }
 
     render() {
         const { content } = this.props;
+        const { version } = this.state;
 
         return (
             <div>
@@ -30,6 +50,20 @@ export default class App extends Component {
                     <div id="content">
                         {content}
                     </div>
+                    <Footer style={{
+                        textAlign: 'center',
+                        position: 'fixed',
+                        bottom: 0,
+                        left: 0,
+                        width: '100%',
+                        padding: '15px 35px',
+                        background: 'white',
+                    }}
+                    >
+                        Corvo
+                        {version ? ` ${version} ` : ' '}
+                        || Boran Seckin
+                    </Footer>
                 </center>
             </div>
         );
